ca77d4b45549abd26248c46515c1b54b1066098e
Kwangok Ahn <dksrhkddhr06@gmail.com>
2021-01-19 10:48:54 +0900

    이미지 로드 에러 수정

--- pages/products.vue
+++ pages/products.vue
@@ -8,7 +8,7 @@
         <div class="products">
             <div class="contents" v-for="items in product" :key="items">
                 <div class="product_image">
-                    <img :src="items.image">
+                    <img :src="require(`@/assets/images/${items.image}.png`)">
                 </div>
                 <p style="font-size: 1.2rem; overflow: "><strong>{{items.product_name}}</strong></p>
                 <p>즉시결제가 <strong>{{items.instant_pay}}원</strong></p>
@@ -26,13 +26,13 @@ export default {
         return {
             categories: ["예술품", "생물", "골동품", "옷"],
             product: [
-                { image: "images/default.png", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0},
-                { image: "images/default.png", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
-                { image: "images/default.png", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
-                { image: "images/default.png", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
-                { image: "images/default.png", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
-                { image: "images/default.png", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
-                { image: "images/default.png", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 }
+                { image: "default", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0},
+                { image: "default", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
+                { image: "default", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
+                { image: "default", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
+                { image: "default", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
+                { image: "default", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 },
+                { image: "default", product_name: "이름이름 default", instant_pay: 0, current_bid_price: 0, bid: 0, time_remain: '00:00:00', see: 0 }
             ]
         };
     },
@@ -77,36 +77,35 @@ export default {
     }
 
     .products {
-    display: flex;
-    flex-wrap: wrap;
-}
+        display: flex;
+        flex-wrap: wrap;
+    }
 
-.contents {
-    margin: 5px;
-    padding: 5px;
-    border: 1px solid black;
-}
+    .contents {
+        margin: 5px;
+        padding: 5px;
+        border: 1px solid black;
+    }
 
-.contents img{
-    display: block; 
-    margin: 0px auto;
-    width: 150px;
-    height: 150px;
-}
+    .contents img{
+        display: block; 
+        margin: 0px auto;
+        width: 150px;
+        height: 150px;
+    }
 
-.product_image {
-    background-color: rgb(245, 245, 245);
-    
-    overflow: hidden;
-}
+    .product_image {
+        background-color: rgb(245, 245, 245);
+        overflow: hidden;
+    }
 
-.product_image img {
-    object-fit: cover;
-    vertical-align: center;
-}
+    .product_image img {
+        object-fit: cover;
+        vertical-align: center;
+    }
 
-.contens a:hover {
-    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
-}
+    .contens a:hover {
+        box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    }
 
 </style>
